6ecda33eb28306ce83cbc0d6d47aa9b35dd781bb
lixnso <lixinso@gmail.com>
2011-08-04 10:07:24 +0800

    FSDataInputStream seek

--- hadoop/src/FileSystemCat.java
+++ hadoop/src/FileSystemCat.java
@@ -1,8 +1,7 @@
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
@@ -18,9 +17,12 @@ public class FileSystemCat {
 		 String uri = args[0];
 		 Configuration conf = new Configuration();
 		 FileSystem fs = FileSystem.get(URI.create(uri),conf);
-		 InputStream in = null;
+		 //InputStream in = null;
+		 FSDataInputStream in = null;
 		 try{
 			 in = fs.open(new Path(uri));
+			 IOUtils.copyBytes(in, System.out, 4096,false);
+			 in.seek(0);
 			 IOUtils.copyBytes(in, System.out, 4096,false);
 		 }finally{
 			 IOUtils.closeStream(in);
